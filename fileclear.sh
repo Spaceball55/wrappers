2f5a48526e202dda3cb19b3bebe3fa28f0e559a0
David Rauch <Davespeed50@gmail.com>
2023-06-13 17:00:56 -0500

    add help to fileclear.sh

--- fileclear.sh
+++ fileclear.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+# David Rauch
+# email: david.rauch@bcm.edu
+# Baylor College of Medicine MHG
+# 6/13/2023
+
+###
+
+# fileclear.sh "$dir"
+# Purpose: Clears a directory of all of its files. 
+
 # inputs:
-# "dir" -> the directory you wish to clear
+# "$dir" -> the directory you wish to clear
+# outputs: N/A, clears te file out of the directory
+
+# How to use:
+# run the command with the directory you wish to clear as its argument (either relative or absolute path)
+# ex:
+# ./fileclear foo
+
+# TROUBLESHOOTING
+# Please make sure you only use 1 directory at a time
+# The script does not access the files of the immediate working directory. 
+#	If using relative paths, access the target directory from its parent directory.
 
 function fileclear() {
 
@@ -9,7 +30,7 @@ ARGS=1 #we can only have 1 argument in this script
 
 if [ $# -ne "$ARGS"  ]
 then
-	echo "Please only pass one argument"
+	echo "Please only pass only one directory"
 	exit 
 fi
 
