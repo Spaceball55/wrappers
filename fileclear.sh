86d0177ecc0c2bf99758aee1584e9cb79e3c9c2e
David Rauch <Davespeed50@gmail.com>
2023-06-13 18:05:26 -0500

    cleanup fileclear.sh

--- fileclear.sh
+++ fileclear.sh
@@ -8,8 +8,7 @@
 ###
 
 # fileclear.sh "$dir"
-# Purpose: Clears a directory of all of its files. 
-
+# Purpose: Clears a directory of all of its files. Run this wrapper in the parent directory of the directory you wish to clear.
 # inputs:
 # "$dir" -> the directory you wish to clear
 # outputs: N/A, clears te file out of the directory
@@ -19,43 +18,43 @@
 # ex:
 # ./fileclear foo
 
-# TROUBLESHOOTING
+# TROUBLESHOOTING:
 # Please make sure you only use 1 directory at a time
 # The script does not access the files of the immediate working directory. 
 #	If using relative paths, access the target directory from its parent directory.
 
 function fileclear() {
 
-ARGS=1 #we can only have 1 argument in this script
-
+#check to make sure that the correct number of arguments are being passed
+ARGS=1 #this function only allows for 1 argument
 if [ $# -ne "$ARGS"  ]
 then
 	echo "Please only pass only one directory"
 	exit 
 fi
 
+#check to see if the directory exists
 if [ -d "$1" ]
 then
 	cd "$1"
 	
-	# if empty
+	# if the directory is empty
 	if [[ $(ls -A | wc -l) -eq 0 ]]
 	then
-		echo 'empty directory'
+		echo "$1 is already empty"
 		exit 999
 	fi
 	
-	files=$(ls -l | wc -l)
+	numfiles=$(ls -l | wc -l) #counts the number
 	rm *
 	echo "$1 cleared"
 		
-	echo "$files files cleared"
+	echo "$numfiles files cleared"
 
 	else
-		echo "out_slurm does not exist"
+		echo "$1 does not exist"
 		exit 999 
 fi
-
 }
 
 fileclear "$@"
